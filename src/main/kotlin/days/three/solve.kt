c6738955c174de6adcb8777218ff7e433324467c
phosfox <mordredur@googlemail.com>
2021-12-03 23:37:42 +0100

    refactoring

--- src/main/kotlin/days/three/solve.kt
+++ src/main/kotlin/days/three/solve.kt
@@ -29,13 +29,6 @@ fun mostFrequent(ls: List<List<Int>>, colIndex: Int): Int? {
     return column(ls, colIndex).groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
 }
 
-fun temp(ls: List<List<Int>>, colIndex: Int): Int {
-    val counts = column(ls, colIndex).groupingBy { it }.eachCount()
-    val zero = Pair(0, counts.getValue(0))
-    val one = Pair(1, counts.getValue(1))
-    return if (zero.second >= one.second) zero.first else one.first
-}
-
 fun leastFrequent(ls: List<List<Int>>, colIndex: Int): Int? {
     return column(ls, colIndex).groupingBy { it }.eachCount().minByOrNull { it.value }?.key
 }
@@ -44,20 +37,12 @@ fun frequencies(ls: List<Int>): Map<Int, Int> {
    return ls.groupingBy { it }.eachCount()
 }
 
-fun calcGammaRate(ls: List<List<Int>>): Int {
-    val mostFreqs = arrayListOf<Int?>()
+fun rate(ls: List<List<Int>>, freq: (ls: List<List<Int>>, colIndex: Int) -> Int?): Int {
+    val freqs = arrayListOf<Int?>()
     for (i in 0 until ls.first().size) {
-        mostFreqs.add(mostFrequent(ls, i))
+        freqs.add(freq(ls, i))
     }
-    return mostFreqs.joinToString("").toInt(2)
-}
-
-fun calcEpsilonRate(ls: List<List<Int>>): Int {
-    val leastFreq = arrayListOf<Int?>()
-    for (i in 0 until ls.first().size) {
-        leastFreq.add(leastFrequent(ls, i))
-    }
-    return leastFreq.joinToString("").toInt(2)
+    return freqs.joinToString("").toInt(2)
 }
 
 fun oxyFilterCriterium(ls: List<Int>): Int {
@@ -74,34 +59,24 @@ fun co2FilterCriterium(ls: List<Int>): Int {
     return  if (mostCommon?.value == leastCommon?.value) 0 else leastCommon?.key!!
 }
 
-fun oxygenRating(ls: List<List<Int>>): List<Int> {
+fun rating(ls: List<List<Int>>, criteria: (ls: List<Int>) -> Int): List<Int> {
     var ret = ls
     for(i in ls.first().indices) {
         if (ret.size == 1) return ret.flatten()
-        val pred = oxyFilterCriterium(column(ret, i))
-         ret = ret.filter { it[i] == pred}
-    }
-    return ret.flatten()
-}
-
-fun co2Rating(ls: List<List<Int>>): List<Int> {
-    var ret = ls
-    for(i in ls.first().indices) {
-        if (ret.size == 1) return ret.flatten()
-        val pred = co2FilterCriterium(column(ret, i))
+        val pred = criteria(column(ret, i))
         ret = ret.filter { it[i] == pred}
     }
     return ret.flatten()
 }
 
 fun part1() {
-    val gammaRate = calcGammaRate(binaries)
-    val epsilonRate = calcEpsilonRate(binaries)
+    val gammaRate = rate(binaries, ::mostFrequent)
+    val epsilonRate = rate(binaries, ::leastFrequent)
     println(gammaRate * epsilonRate)
 }
 
 fun part2() {
-    val oxy = oxygenRating(binaries).joinToString("").toInt(2)
-    val co2 = co2Rating(binaries).joinToString("").toInt(2)
+    val oxy = rating(binaries, ::oxyFilterCriterium).joinToString("").toInt(2)
+    val co2 = rating(binaries, ::co2FilterCriterium).joinToString("").toInt(2)
     println(oxy * co2)
 }
