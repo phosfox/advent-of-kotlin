35a6da5ba19094693f652e96a743f76518d8302e
phosfox <mordredur@googlemail.com>
2021-12-10 20:52:15 +0100

    day10

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import days.nine.part1
-import days.nine.part2
+import days.ten.part1
+import days.ten.part2
 
 
 fun main() {
---
+++ src/main/kotlin/days/ten/solve.kt
@@ -0,0 +1,77 @@
+package days.ten
+
+import java.io.File
+import java.util.ArrayDeque
+
+typealias Stack<E> = MutableList<E>
+
+fun <E> Stack<E>.peek(): E? {
+    return this.getOrNull(0)
+}
+
+fun <E> Stack<E>.pop(): E? {
+    return this.removeFirstOrNull()
+}
+
+val testInput = File("src/main/kotlin/days/ten/test-input.txt").readLines()
+val input = File("src/main/kotlin/days/ten/input.txt").readLines()
+
+val pairs = mapOf(
+    '(' to ')', '{' to '}', '[' to ']', '<' to '>', ')' to '(', '}' to '{', ']' to '[', '>' to '<'
+)
+
+
+fun matching(left: Char, right: Char) = pairs[left] == right
+
+val opening = setOf('(', '{', '[', '<')
+val closing = setOf(')', '}', ']', '>')
+
+fun score(line: String): Int {
+    val scores = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+    val stack = ArrayDeque<Char>()
+    line.forEach {
+        if (it in opening) {
+            stack.push(it)
+        }
+        if (it in closing) {
+            val ele = stack.pop() ?: return 0
+            if (!matching(it, ele)) {
+                return scores.getOrDefault(it, 0)
+            }
+        }
+    }
+    return 0
+}
+
+fun closingChars(line: String): ArrayDeque<Char> {
+    val stack = ArrayDeque<Char>()
+    line.forEach {
+        if (it in opening) {
+            stack.push(it)
+        }
+        if (it in closing) {
+            val ele = stack.peek() ?: return stack
+            if (!matching(it, ele)) {
+                return stack
+            }
+           stack.pop()
+        }
+    }
+    return stack
+}
+
+fun completionScore(ls: List<Char?>): Long {
+    val scores = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+    return ls.fold(0) { acc, c -> (acc * 5) + scores.getOrDefault(c, 0) }
+}
+
+fun part1() {
+    println(input.sumOf { score(it) })
+}
+
+fun part2() {
+    val incomplete = input.filter { score(it) == 0 }
+    val res = incomplete.map { it -> closingChars(it).map { pairs[it] } }.map { completionScore(it) }
+    val winner = res.sorted()[res.size / 2]
+    println(winner)
+}
