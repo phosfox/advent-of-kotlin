87bc5037ce500a15be5205b37e2ddd9a60429317
phosfox <mordredur@googlemail.com>
2021-12-12 23:45:24 +0100

    day12-2

--- src/main/kotlin/days/twelve/solve.kt
+++ src/main/kotlin/days/twelve/solve.kt
@@ -17,7 +17,7 @@ fun isSmallCave(s: String) = s.first().isLowerCase()
 fun dfs(graph: Graph, start: String): MutableList<List<String>> {
     val paths = mutableListOf<List<String>>()
     fun helper(start: String, path: List<String>) {
-        for(w in graph[start]!!) {
+        for (w in graph[start]!!) {
             if (isSmallCave(w) && path.contains(w) || w == "start") continue
             if (w == "end") {
                 paths.add(path.plus("end"))
@@ -30,17 +30,49 @@ fun dfs(graph: Graph, start: String): MutableList<List<String>> {
     return paths
 }
 
+fun dfs2(graph: Graph, start: String, smallCaves: Set<String>): MutableList<List<String>> {
+    val paths = mutableListOf<List<String>>()
+    fun helper(start: String, path: List<String>, canTwice: String) {
+        for (w in graph[start]!!) {
+            if (w == "start") continue
+            if (path.count { it == canTwice } > 2) continue
+            if (w != canTwice && isSmallCave(w) && path.contains(w)) continue
+            if (w == "end") {
+                paths.add(path.plus("end"))
+                continue
+            }
+            helper(w, path.plus(w), canTwice)
+        }
+    }
+    smallCaves.forEach { sc ->
+        helper(start, listOf(), sc)
+    }
+    return paths
+}
+
 private fun part1() {
     val graph = parse("src/main/kotlin/days/twelve/input.txt", {
         val (k, v) = it.split("-")
         listOf(k to v, v to k)
     }).flatten().groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
-    println(graph)
     val paths = dfs(graph, "start")
     println(paths.size)
 
 }
 
+private fun part2() {
+    val graph = parse("src/main/kotlin/days/twelve/input.txt", {
+        val (k, v) = it.split("-")
+        listOf(k to v, v to k)
+    }).flatten().groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
+    println(graph)
+    val smallCaves = graph.keys.filter { it.first().isLowerCase() && it != "start" && it != "end" }.toSet()
+    val paths = dfs2(graph, "start", smallCaves)
+    println(paths.toSet().size)
+
+}
+
 fun solve() {
     part1()
+    part2()
 }
