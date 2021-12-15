1df8d2d94b3c62c872f33efe03c37ee700c7bf87
phosfox <mordredur@googlemail.com>
2021-12-16 00:17:40 +0100

    day15 done

--- src/main/kotlin/days/_15/solve.kt
+++ src/main/kotlin/days/_15/solve.kt
@@ -2,28 +2,22 @@ package days._15
 
 import utils.Grid
 import utils.Point
-import utils.ints
 import utils.parse
 import java.lang.Double.POSITIVE_INFINITY
 import java.util.*
 
 
-fun dijkstra(g: Grid<Int>, source: Point): Pair<MutableMap<Point, Int>, MutableMap<Point, Point?>> {
-    val distances: MutableMap<Point, Int> = mutableMapOf()
-    val previous: MutableMap<Point, Point?> = mutableMapOf()
-    distances[source] = 0
-    data class Node(val p: Point, val risk: Int)
+fun dijkstra(g: Grid<Long>, source: Point): Pair<MutableMap<Point, Long>, MutableMap<Point, Point>> {
+    val distances: MutableMap<Point, Long> = g.map.keys.associateWith { 100000L }.toMutableMap()
+    val previous: MutableMap<Point, Point> = mutableMapOf()
+    distances[source] = g.map.getOrDefault(source, 9L)
+    data class Node(val p: Point, val risk: Long)
 
     val compByRisk: Comparator<Node> = compareBy { it.risk }
     val q: PriorityQueue<Node> = PriorityQueue<Node>(compByRisk)
 
     g.map.keys.forEach {
-        if (it != source) {
-            distances[it] = 100000
-            previous[it] = null
-
-        }
-        q.add(Node(it, distances.getOrDefault(it, 10000)))
+        q.add(Node(it, distances.getOrDefault(it, POSITIVE_INFINITY.toLong())))
     }
 
     while (q.isNotEmpty()) {
@@ -52,9 +46,35 @@ fun shortestPath(to: Point, previous: Map<Point, Point?>): MutableList<Point> {
     }
 }
 
+fun wrapAdd(l: Long, add: Long): Long {
+    var ret = l
+    for (i in 0 until add) {
+        ret = (ret + 1) % 10
+        if (ret == 0L) {
+            ret = 1L
+        }
+    }
+    return ret
+}
+
+fun mapInc(ls: List<Long>): List<Long> {
+    return (0..4L).map { num ->
+        ls.map {
+            wrapAdd(it, num)
+        }
+    }.flatten()
+}
+
+fun gridInc(grid: List<List<Long>>): List<List<Long>> {
+    val incGrid =
+        (0..4L).map { num -> grid.map { mapInc(it) }.flatten().map { wrapAdd(it, num) }.chunked(grid.first().size * 5) }
+    return incGrid.flatten()
+}
+
+val input = parse("src/main/kotlin/days/_15/input.txt", { line -> line.chunked(1).map { it.toLong() } })
+
 fun solve() {
-    val input = parse("src/main/kotlin/days/_15/input.txt", { line -> line.chunked(1).map { it.toInt() } })
-    val m = mutableMapOf<Point, Int>()
+    val m = mutableMapOf<Point, Long>()
     input.forEachIndexed { y, row ->
         row.forEachIndexed { x, risk ->
             m[Point(x, y)] = risk
@@ -65,7 +85,29 @@ fun solve() {
     val target = Point(g.width, g.height)
     val (dist, prev) = dijkstra(g, start)
     val path = shortestPath(target, prev)
-    val risk = path.sumOf { g.map[it]!! }
-    println(risk - 1)
+    val risk = path.dropLast(1).sumOf { g.map[it] ?: 0 }
+    println(risk)
+
+
+    part2()
+
+
 }
 
+fun part2() {
+
+    val hugeMap = mutableMapOf<Point, Long>()
+    val hugeInput = gridInc(input)
+    hugeInput.forEachIndexed { y, row ->
+        row.forEachIndexed { x, risk ->
+            hugeMap[Point(x, y)] = risk
+        }
+    }
+    val hugeG = Grid(hugeMap)
+    val hugeStart = Point(0, 0)
+    val hugeTarget = Point(hugeG.width, hugeG.height)
+    val (hugeDist, hugePrev) = dijkstra(hugeG, hugeStart)
+    val hugePath = shortestPath(hugeTarget, hugePrev)
+    val hugeRisk = hugePath.dropLast(1).sumOf { hugeG.map[it] ?: 0 }
+    println(hugeRisk)
+}
